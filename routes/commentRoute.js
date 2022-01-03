3fba097d214c3a33caf42596cf036b4743f41a97
Mohammed Rakib <mohammedrakib81336@gmail.com>
2022-01-03 19:46:43 +0600

    comment model design

--- routes/commentRoute.js
+++ routes/commentRoute.js
@@ -2,22 +2,34 @@ const express = require("express");
 const router = express.Router();
 const { Comment } = require("../models/comments");
 
-// get all comments by blogId
-router.get("/:id", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const id = req.params.id;
-    const comments = await Comment.find({ blogId: id });
-    res.json(comments);
+    const result = await Comment.find({});
+    res.json({
+      count: result.length,
+      data: result,
+    });
   } catch (err) {
     res.status(500).json(err.message);
   }
 });
 
-// post a comment
+// add a comment from user and store it into database
 router.post("/", async (req, res) => {
   try {
     const comment = new Comment(req.body);
     const result = await comment.save();
+    res.json(result);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
+// delete comments
+router.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await Comment.findByIdAndDelete(id);
     res.json(result);
   } catch (err) {
     res.status(500).json(err.message);
